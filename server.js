b684c551095524d37cf0aeccbf2118459f691945
Luciana Bruscino <luciana.bruscino@gmail.com>
2018-04-23 11:49:25 -0400

    add server timeout to 30000

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ var app = require('./src/app/index');
 
 var port = process.env.PORT || 8080;
 
-app.listen(port, function() {
+var server = app.listen(port, function() {
     console.log("server running on port", port);
 });
+
+server.timeout = 30000; //0.5 min 
