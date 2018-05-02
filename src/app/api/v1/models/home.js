5d153ddf7206b9a5e5395042853318f0d3d4d92b
Luciana Bruscino <luciana.bruscino@gmail.com>
2018-05-01 22:35:37 -0400

    clear cache on error

--- src/app/api/v1/models/home.js
+++ src/app/api/v1/models/home.js
@@ -7,7 +7,8 @@
 
 'use strict'
 
-var Observable = require('rxjs/Observable').Observable;
+var Observable = require('rxjs/Observable').Observable,
+    client = require('../models/client');
 require('rxjs/add/observable/of');
 require('rxjs/add/operator/map');
 require('rxjs/add/observable/forkJoin');
@@ -28,17 +29,62 @@ const favoriteService = require('../services/favorite');
 const favoriteMapping = require('../mappings/favorite');
 const loggingService = require('../services/logging');
 
-class home {
-    getAccount(req, res){
+const REDIS_HOME_CACHE = "alop-adapter-home";
+const REDIS_HOME_CACHE_TIME = 6;
+
+let home = {
+    hasError: false
+};
+
+home.get = function(req, res, next){
+       var account = {};
+        client.get(REDIS_HOME_CACHE, (error, result) => {
+            if(result && !home.hasError){
+                console.log("data from caching");
+                res.status(200);
+                res.json(JSON.parse(result));
+            } else {
+                console.log("data from API directly");
+                home.getAccount(req, res)
+                .subscribe(
+                    (value) => {
+                        try{
+                            account = Object.assign(value, account);
+                        }catch(error){
+                            home.hasError = true;
+                             let msg = { message: 'Account Subscriber Error Message: ' + error };
+                             res.status(500);
+                             res.json(msg);
+                        }                        
+                    },
+                    (error) => {     
+                        home.hasError = true;                   
+                        let msg = { message: 'Subscriber Error Message: ' + error };
+                        res.status(500);
+                        res.json(msg);
+                    },
+                    () => {         
+                        client.setex(REDIS_HOME_CACHE, REDIS_HOME_CACHE_TIME, JSON.stringify(account));
+                        res.status(200);                        
+                        res.json(account);
+                    }
+                );
+            }
+        });
+    }
+
+home.getAccount = function(req, res){
     	const u = userService.get(req.headers)  
                 .map((data) => {                    
                     if (data.statusCode && data.statusCode > 400){
+                        home.hasError = true;
                         loggingService.logError(data, "User Service API");
                     }
                     return data
                 })               
-                 .map((data) => ({user: userMapping.transform(data)}))              
+                 .map((data) => ({user: userMapping.transform(data)}))
                  .catch((error) => {
+                    home.hasError = true;
                     loggingService.logError(error, "User Service Transform ");
                     return Observable.of({
                         user: {}
@@ -51,13 +97,15 @@ class home {
 
         const w = workoutService.get(req.headers)
                     .map((data) => {                       
-                        if (!data || data.statusCode > 400){                             
-                            loggingService.logError(data, "Workout Service API");
+                        if (!data || data.statusCode > 400){                           
+                            home.hasError = true;
+                            loggingService.logError(data, "Workout Service API");          
                         }                                
                         return data
                     })
                     .map((data) => ({workout: workoutMapping.transform(data)}))
                     .catch((error) => {
+                        home.hasError = true;
                         loggingService.logError(error, "Workout Service Transform");
                         return Observable.of({
                             workouts: {}
@@ -66,7 +114,8 @@ class home {
 
         const a = trackingService.get(req.headers)
             .map((data) => {                       
-                        if (!data || data.statusCode > 400){                             
+                        if (!data || data.statusCode > 400){       
+                            home.hasError = true;                      
                             loggingService.logError(data, "Tracking Service API");
                         }                                
                         return data
@@ -74,6 +123,7 @@ class home {
             .map((data) => ({
                 activities: activityMapping.transform(data)
             })).catch((error) => {
+                home.hasError = true;
                 return Observable.of({
                     activities: {}
                 })
@@ -81,7 +131,8 @@ class home {
 
         const f = favoriteService.get(req.headers)
             .map((data) => {                       
-                        if (!data || data.statusCode > 400){                             
+                        if (!data || data.statusCode > 400){            
+                            home.hasError = true;                 
                             loggingService.logError(data, "Favorite Service API");
                         }                                
                         return data
@@ -89,6 +140,7 @@ class home {
             .map((data) => ({
                 favorites: favoriteMapping.transform(data)
             })).catch((error) => {
+                home.hasError = true;
                 return Observable.of({
                     favorites: {}
                 })
@@ -96,7 +148,8 @@ class home {
 
         const m = meditationService.get(req.headers)
             .map((data) => {                       
-                if (!data || data.statusCode > 400){                             
+                if (!data || data.statusCode > 400){
+                    home.hasError = true;                 
                     loggingService.logError(data, "Meditation Service API");
                 }                                
                 return data
@@ -104,6 +157,7 @@ class home {
             .map((data) => ({
                     meditation: meditationMapping.transform(data)
                 })).catch((error) => {
+                    home.hasError = true;
                     return Observable.of({
                         meditation: {}
                     })
@@ -111,5 +165,5 @@ class home {
 
 		return Observable.concat(m, Observable.forkJoin(f, a, wl,w,u).concatMap(results => Observable.from(results)));
     }
-}
-module.exports = new home();
+
+module.exports = home;
--- src/app/api/v1/routers/home.js
+++ src/app/api/v1/routers/home.js
@@ -8,11 +8,9 @@
 'use strict'
 
 var router = require('express').Router(),
-    home = require('../models/home'),
-    client = require('../models/client');
+    home = require('../models/home');
 
-const REDIS_HOME_CACHE = "alop-adapter-home";
-const REDIS_HOME_CACHE_TIME = 6;
+
 
 router.use((req, res, next) => {
     // access the req.params object
@@ -20,39 +18,6 @@ router.use((req, res, next) => {
     // do logging
     next();
 });
-router.get('/home', (req, res, next) => {
-	   var account = {};
-        client.get(REDIS_HOME_CACHE, (error, result) => {
-            if(result){
-                res.status(200);
-                res.json(JSON.parse(result));
-            } else {
-                home.getAccount(req, res)
-                .subscribe(
-                    (value) => {
-                        try{                           
-                            account = Object.assign(value, account);
-                        }catch(error){
-                             let msg = { message: 'Account Subscriber Error Message: ' + error };
-                             res.status(500);
-                             res.json(msg);
-                        }                        
-                    },
-                    (error) => {                        
-                        let msg = { message: 'Subscriber Error Message: ' + error };
-                        res.status(500);
-                        res.json(msg);
-                    },
-                    () => {         
-                        client.setex(REDIS_HOME_CACHE, REDIS_HOME_CACHE_TIME, JSON.stringify(account));
-                        res.status(200);                        
-                        res.json(account);
-                    }
-                );
-            }
-        });
-
-    	
-});
+router.get('/home', home.get);
 
 module.exports = router;
