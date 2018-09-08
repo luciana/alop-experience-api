7f824f121665d3893e2f2f4e1b6469f8b4d89938
Luciana Bruscino <luciana.bruscino@gmail.com>
2018-09-08 10:42:54 -0400

    schedule api for id

--- src/app/api/v1/schedule/router.js
+++ src/app/api/v1/schedule/router.js
@@ -10,6 +10,7 @@
 var router = require('express').Router();
 const Observable = require('rxjs/Observable').Observable;
 const scheduleMapping = require('../shared/mappings/schedule');
+const SCHEDULE_MAX = 25;
 
 router.use((req, res, next) => {
     // access the req.params object
@@ -27,9 +28,9 @@ router.get('/schedule', (req, res, next) => {
 
 router.get('/schedule/:id', (req, res, next) => {
 	let id = req.params.id;
-	if (!id) id = 2;
-	console.log("ADAADA", id);
-
+	if ((!id) || (!id.match(/^([1-9]|10)$/)) || (id > SCHEDULE_MAX)) {
+		id = Math.floor(Math.random() * Math.floor(SCHEDULE_MAX+1));
+	}
 	let schedule = scheduleMapping.getById(id);
 	res.status(200);
 	res.json(schedule);
