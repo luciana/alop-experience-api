aa7c83edfbef6797c3130a7d2ae9503502a9f04b
Luciana Bruscino <luciana.bruscino@gmail.com>
2018-09-30 08:30:35 -0400

    switched progress to month instead of week numbers

--- src/app/api/v1/shared/mappings/activity.js
+++ src/app/api/v1/shared/mappings/activity.js
@@ -112,9 +112,9 @@ activity.transform = (data) =>{
         let results = {};
         let result = {};
         var m = new mappings();        
-        result.progress_label = "THIS WEEK";
-        result.classes_taken = m.get_classes_taken_this_week(data);
-        result.minutes_taken = m.get_minutes_taken_this_week(data);
+        result.progress_label = "THIS MONTH";
+        result.classes_taken = m.get_classes_taken_this_month(data);
+        result.minutes_taken = m.get_minutes_taken_this_month(data);
         result.classes_taken_this_week = m.get_classes_taken_this_week(data);
         result.minutes_taken_this_week = m.get_minutes_taken_this_week(data);
         result.classes_taken_this_month = m.get_classes_taken_this_month(data);
