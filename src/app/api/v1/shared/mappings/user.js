5d643e26a2a29d8c8272198319bfc3e7ee8f3d8c
Luciana Bruscino <luciana.bruscino@gmail.com>
2019-03-29 21:37:11 -0400

    fix for the plan info information for users paying with inapp

--- src/app/api/v1/shared/mappings/user.js
+++ src/app/api/v1/shared/mappings/user.js
@@ -39,8 +39,9 @@ user.transform = (data) => {
         var planActionId = 0;
         const trial = new Date(data.trial_end_date);       
         if(user.isPaid(result)){
-            const active_until =  new Date(result.subscriptions[0].active_until);            
-            if(isNaN(new Date(active_until))){
+            const active_until =  new Date(result.subscriptions[0].active_until);     
+            console.log("active until value" , result.subscriptions[0].active_until);       
+            if(active_until!= null){
                 info = "Subscription active until " + (active_until.getMonth() + 1) + '/' + active_until.getDate() + '/' +  active_until.getFullYear();
                 planAction = "";
             }else{
