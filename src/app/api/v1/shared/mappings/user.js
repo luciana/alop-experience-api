f65157944c34e2502652e43818323bc83ec7f749
Luciana Bruscino <luciana.bruscino@gmail.com>
2020-06-01 00:14:45 -0400

    text

--- src/app/api/v1/shared/mappings/user.js
+++ src/app/api/v1/shared/mappings/user.js
@@ -29,7 +29,7 @@ user.transform = (data) => {
         result.name = data.name || "Friend";        
         result.email = data.email || "";
         result.sign_in_count = data.sign_in_count || 1;
-        result.greeting_text = "Join us for LIVE classes on ALotOfPilates.com"; //user.timeGreeting(result.name);
+        result.greeting_text = "Join us for LIVE classes on ALotOfPilates.com"; 
         result.created_at = data.created_at || new Date().toISOString();
         result.location = data.location || null;
         result.subscriptions = data.subscriptions || defaultSubs;
